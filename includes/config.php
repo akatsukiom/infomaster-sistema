aae7ab40c22c878b4898e3feea0cdba2a9edd80a
akatsukiom <akatsukiomeg@gmail.com>
2025-04-19 01:45:01 -0600

    Cambios en config.php y ajustes en estructura

--- includes/config.php
+++ includes/config.php
@@ -6,9 +6,9 @@ if(!defined('ACCESO_PERMITIDO')) {
 
 // Configuración de la base de datos
 define('DB_HOST', 'localhost');
-define('DB_USER', 'tu_usuario');
-define('DB_PASS', 'tu_contraseña');
-define('DB_NAME', 'infomaster_db');
+define('DB_USER', 'crist110_crist110_admin');
+define('DB_PASS', 'Cuenta2022$');
+define('DB_NAME', 'crist110_infomaster_db');
 
 // Conexión a la base de datos
 $conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -20,7 +20,7 @@ if ($conexion->connect_error) {
 $conexion->set_charset("utf8");
 
 // Configuración global del sitio
-define('URL_SITIO', 'http://infomaster.com.mx/');
+define('URL_SITIO', 'https://cristianalejandro1744816593450.2130068.misitiohostgator.com/');
 define('MONEDA', '$');
 
 // Iniciar sesión si no está iniciada
